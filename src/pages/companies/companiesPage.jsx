070da75ee70c6d6adbc4a8b0467cb38c6c267891
SimeonI <simeon.i@appgr8.com>
2020-12-10 11:41:33 +0200

    m

--- src/pages/companies/companiesPage.jsx
+++ src/pages/companies/companiesPage.jsx
@@ -1,16 +1,33 @@
+import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import Loading from "../../components/loading/loading";
+import { NavLink } from "react-router-dom";
+import "./companies.scss";
+
 const CompaniesPage = () => {
-  // return (
-  //   <div className="companies">
-  //     <h1 className="h1">The whole list of companies: </h1>
-  //     {companies.map(c => (
-  //       <NavLink to={`/company/${c.id}`}>
-  //         <span className="h3">Company:</span>
-  //         <span className="each_company">{c.name}</span>
-  //       </NavLink>
-  //     ))}
-  //   </div>
-  // );
-  return <h1>companies page</h1>;
+  const [companies, setCompanies] = useState([]);
+
+  const companiesSelector = useSelector(state => state.companies.companies);
+
+  useEffect(() => {
+    setCompanies(companiesSelector);
+  }, []);
+
+  if (companies.length === 0) {
+    return <Loading />;
+  }
+
+  return (
+    <div className="companies">
+      <h1 className="h1">The whole list of companies: </h1>
+      {companies.map(c => (
+        <NavLink to={`/company/${c.id}`}>
+          <span className="h3">Company:</span>
+          <span className="each_company">{c.name}</span>
+        </NavLink>
+      ))}
+    </div>
+  );
 };
 
 export default CompaniesPage;
